9c395d050329eb319df61b7e8d95f9cf8a3ec281
Jacky <jackyliang@berkeley.edu>
2016-11-04 10:46:54 -0700

    working ui w/o service

--- scripts/yumi_teleop_client.py
+++ scripts/yumi_teleop_client.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 Script to be ran on the 'client' machine on a masters-YuMi teleop setup. Client
 machine is the computer connected to the masters' vision system.
@@ -10,24 +11,17 @@ from multiprocess import Process, Queue
 
 from masters_control.srv import str_str
 
-class _UI(Process):
+class UI(Process):
 
     BG_COLOR = (0,0,255)
     TXT_COLOR = (0,0,0)
     HI_COLOR = (0,255,255)
 
-    def __init__(self, req_q, res_q):
+    def __init__(self, cid):
         Process.__init__(self)
-        self.cam = cv2.VideoCapture(0)
-        self.left = cv2.namedWindow("left")
-        self.right = cv2.namedWindow("right")
-
-        self.req_q = req_q
-        self.res_q = res_q
-
-        self.list_view = []
-        self.list_index = 0
-        self.show_overlay = True
+        self.req_q = Queue()
+        self.res_q = Queue()
+        self.cid = cid
 
     def gen_list_view(self, frame):
         overlay = frame.copy()
@@ -43,16 +37,23 @@ class _UI(Process):
             bottom_right = tuple(o_bottom_right + i * delta)
             text_left = tuple(o_text_left + i * delta)
 
-            rec_color = _UI.HI_COLOR if i == self.list_index else _UI.BG_COLOR
+            rec_color = UI.HI_COLOR if i == self.list_index else UI.BG_COLOR
 
             cv2.rectangle(overlay, top_left, bottom_right, rec_color,-1)
-            cv2.putText(overlay, item, text_left, cv2.FONT_HERSHEY_SIMPLEX, 0.7, _UI.TXT_COLOR, 1)
+            cv2.putText(overlay, item, text_left, cv2.FONT_HERSHEY_SIMPLEX, 0.7, UI.TXT_COLOR, 1)
 
         cv2.putText(overlay, "AUTOLab YuMi Teleop Interface", (20, overlay.shape[0]-10), cv2.FONT_HERSHEY_PLAIN, 1.0, (255,255,255), 1)
 
         return overlay
 
     def run(self):
+        self.cam = cv2.VideoCapture(self.cid)
+        self.left = cv2.namedWindow("left")
+        self.right = cv2.namedWindow("right")
+
+        self.list_view = []
+        self.list_index = 0
+        self.show_overlay = True
         while True:
             _, frame = self.cam.read()
 
@@ -87,54 +88,45 @@ class _UI(Process):
             if pressed == ord('a'):
                 self.show_overlay = not self.show_overlay
 
-        self.stop()
-
-    def stop(self):
         self.cam.release()
         cv2.destroyAllWindows()
 
-class UI:
-
-    def __init__(self):
-        self._req_q = Queue()
-        self._res_q = Queue()
-        self._ui = _UI(self._req_q, self._res_q)
-        self._ui.start()
-
     def list_view(self, lst):
-        self._req_q.put(("list_view", lst))
-        while self._res_q.empty():
+        self.req_q.put(("list_view", lst))
+        while self.res_q.empty():
             pass
-        return self._res_q.get()
+        return self.res_q.get()
 
     def set_overlay(self, overlay):
-        self._req_q.put(("overlay", overlay))
+        self.req_q.put(("overlay", overlay))
 
     def stop(self):
-        self._req_q.put(("stop"))
+        self.req_q.put(("stop",))
 
 class YuMiTeleopClient:
 
-    def __init__(self):
+    def __init__(self, cid):
         self.menu_main = ("Collect Demos", "Sandbox", "Quit")
         self.menu_pause_teleop = ("Pause", "Finish")
         self.menu_resume_teleop = ("Resume", "Finish")
         self.menu_demo = None
 
-        self.ui = _UI(self.menu_main)
+        self.ui = UI(cid)
         #TODO: Subscribe to gripper events and republish
 
     def run(self):
+        '''
         rospy.loginfo("Init YuMiTeleopClient")
         rospy.init_node("yumi_teleop_client")
-        rospy.wait_for_service('yumi_teleop_host_ui_service')
-        self.ui_service = rospy.ServiceProxy('yumi_teleop_host_ui_service', str_str)
-
-        demo_names = eval(self.ui_service('list_demos'))
+        rospy.wait_for_service('yumi_teleop_hostUI_service')
+        self.ui_service = rospy.ServiceProxy('yumi_teleop_host_UI_service', str_str)
+        '''
+        #demo_names = eval(self.ui_service('list_demos'))
+        demo_names = ['a','b','c']
         self.menu_demo = tuple(demo_names) + ("Back",)
 
         "A FSM to interact with the teleop interface"
-        self.ui.run()
+        self.ui.start()
         self.cur_menu = self.menu_main
         self.cur_state = "standby"
         while True:
@@ -146,7 +138,7 @@ class YuMiTeleopClient:
             self.cur_state, self.cur_menu, overlay = transition(ui_input)
             self.ui.set_overlay(overlay)
 
-            if cur_state == "exit":
+            if self.cur_state == "exit":
                 break
         self.ui.stop()
 
@@ -154,7 +146,7 @@ class YuMiTeleopClient:
         if ui_input == "Collect Demos":
             return "demo_selection", self.menu_demo, True
         elif ui_input == "Sandbox":
-            res = self.ui_service("teleop_start")
+            #res = self.ui_service("teleop_start")
             return "teleop", self.menu_pause_teleop, False
         elif ui_input == "Quit":
             return "exit", None, None
@@ -162,25 +154,26 @@ class YuMiTeleopClient:
     def t_demo_selection(self, ui_input):
         if ui_input == "Back":
             return "standby", self.menu_main, True
-        res = self.ui_service("choose_demo", ui_input)
+        #res = self.ui_service("choose_demo", ui_input)
         return "teleop", self.menu_pause_teleop, False
 
     def t_teleop(self, ui_input):
         if ui_input == "Pause":
-            res = self.ui_service("teleop_pause")
+            #res = self.ui_service("teleop_pause")
             return "teleop_paused", self.menu_resume_teleop, True
         elif ui_input == "Finish":
-            res = self.ui_service("teleop_finish")
+            #res = self.ui_service("teleop_finish")
             return "standby", self.menu_main, True
 
-    def t_telop_paused(self, ui_input):
+    def t_teleop_paused(self, ui_input):
         if ui_input == "Resume":
-            res = self.ui_service("teleop_resume")
+            #res = self.ui_service("teleop_resume")
             return "teleop", self.menu_pause_teleop, True
         elif ui_input == "Finish":
-            res = self.ui_service("teleop_finish")
+            #res = self.ui_service("teleop_finish")
             return "standby", self.menu_main, True
 
 if __name__ == "__main__":
-    ytc = YuMiTeleopClient()
+    cid = 0
+    ytc = YuMiTeleopClient(cid)
     ytc.run()
