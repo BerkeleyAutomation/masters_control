6022713d23081bbee98e3d4d1629314cec99fa74
Jacky Liang <jackyliang@berkeley.edu>
2017-03-07 19:29:42 -0800

    separated sh scripts on autoalb1

--- run/autolab1.sh
+++ run/autolab1.sh
@@ -3,17 +3,7 @@ source activate alan_jacky
 echo "Done\!"
 
 echo "Sourcing ros..."
-source "~/Workspace/jacky_working/ros_ws/devel_setup.sh"
+source "~/Workspace/jacky_working/ros_ws/devel/setup.sh"
+echo "Done!"
+
 roscd masters_control
-echo "Done!"
-
-echo "Starting video service..."
-python scripts/video_saving_service.py &
-echo "Done!"
-
-echo "Starting pose mapping service..."
-python scripts/masters_yumi_connector.py &
-echo "Done!"
-
-echo "Serving pose mapping and video service indefinitely..."
-wait 
