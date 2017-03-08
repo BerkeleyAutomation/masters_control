a32ecb015b22df9e51f4fd9cc50659521d97633c
bthananjeyan <brijenthananjeyan@gmail.com>
2017-03-07 21:50:55 -0800

    fixed bug in davinci sh

--- run/daVinci2.sh
+++ run/daVinci2.sh
@@ -1,8 +1,8 @@
 echo "Sourcing jacky_venv..."
-source "~/Envs/jacky_venv/bin/activate"
+source ~/Envs/jacky_venv/bin/activate
 echo "Done!"
 
 echo "Sourcing ros..."
-source "~/Workspace/jacky_working/ros_ws/devel/setup.sh"
+source ~/Workspace/jacky_working/ros_ws/devel/setup.sh
 roscd masters_control
 echo "Done!"
